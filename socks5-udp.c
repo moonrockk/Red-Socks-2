c94ce2ce1fb7f291b4b051c738d84b2eb7842c26
Stanislav <119200481+moonrockk@users.noreply.github.com>
2023-07-19 04:36:22 +0300

    Update socks5-udp.c

--- socks5-udp.c
+++ socks5-udp.c
@@ -205,7 +205,7 @@ static void socks5_read_assoc_reply(struct bufferevent *buffev, void *_arg)
 	}
 
 	if (reply.h.ver != socks5_ver) {
-		redudp_log_error(client, LOG_NOTICE, "Socks5 server reported unexpected reply version: %u", reply.h.ver);
+		redudp_log_error(client, LOG_NOTICE, "Socks5 server reported unexpected reply version: %u", reply);
 		goto fail;
 	}
 
